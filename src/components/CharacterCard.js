c8691199eda6bcce33a9c379e498fb39d2a5c487
RichardEsquivel <rlesquivel@gmail.com>
2019-08-23 12:15:39 -0500

    Have characters appearing on page

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,13 @@
 import React from "react";
+import CharacterList from "./CharacterList";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+export default function CharacterCard({ characters }) {
+	return (
+		<div>
+			<img src={characters.image} />
+			<h1> {characters.name}</h1>
+			<p>{characters.species}</p>
+			<h2>{characters.location.name}</h2>
+		</div>
+	)
+};
